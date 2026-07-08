bcbc99b39284741340bf25424a3c251a6cd5916b
Anqi Xie <anqix@gpu28.int.autonlab.org>
2026-07-08 17:25:51 -0400

    newest

--- scripts/create_stamp_experiment_grid.py
+++ scripts/create_stamp_experiment_grid.py
@@ -230,10 +230,10 @@ for dataset_name in dataset_names:
         }
 
         if temporal_channel_selection is None:
-            exp_name = f'rerun_{embedding_model_name}_nrs{exp_config["n_random_seeds"]}_ne{n_epochs}_D{D}_'
+            exp_name = f'rerun2_{embedding_model_name}_nrs{exp_config["n_random_seeds"]}_ne{n_epochs}_D{D}_'
         else:
             tcs_str = '-'.join([str(tc) for tc in temporal_channel_selection])
-            exp_name = f'rerun_{embedding_model_name}_tcs{tcs_str}_nrs{exp_config["n_random_seeds"]}_ne{n_epochs}_D{D}_'
+            exp_name = f'rerun2_{embedding_model_name}_tcs{tcs_str}_nrs{exp_config["n_random_seeds"]}_ne{n_epochs}_D{D}_'
 
         if initial_proj_params is not None:
             exp_name += 'ip-'
--- scripts/run_embedding_exp_manual.py
+++ scripts/run_embedding_exp_manual.py
@@ -10,7 +10,7 @@ dataset_names = ['sere']
 
 #change before run
 exp_names = [
-'rerun_MOMENT-1-large_nrs5_ne200_D128_ip-full-dr0.3_pe-basicNST_gmlp-tcriss_cross-nl8-dff256-dr0.3_cmconcat_nonrec_mhap-A4-dr0.3-Q8-qcweighted_sum_lres0.1_ls0.1gcT_inorm_tdr1.0_stage2_linear'
+'rerun2_MOMENT-1-large_nrs5_ne200_D128_ip-full-dr0.3_pe-basicNST_gmlp-tcriss_cross-nl8-dff256-dr0.3_cmconcat_nonrec_mhap-A4-dr0.3-Q8-qcweighted_sum_lres0.1_ls0.1gcT_inorm_tdr1.0_stage2_linear'
 ]
 
 experiments_dir = local_config.tsfm_experiments_dir
